74c1da81edb575b9edaaa033da9f5fe91d19c4d4
codnscodns <dnstm1119@naver.com>
2025-01-13 16:19:52 +0900

    [ui] 입출금 내역 페이지

--- woo-ah-hana-web/src/app/ui/components/card-menu.tsx
+++ woo-ah-hana-web/src/app/ui/components/card-menu.tsx
@@ -9,6 +9,7 @@ import IconManagement from '../../assets/img/icon-management.png';
 import IconClosing from '../../assets/img/icon-closing.png';
 import IconFeeCheck from '../../assets/img/icon-fee-check.png';
 import Image, { StaticImageData } from 'next/image';
+import { useRouter } from 'next/navigation';
 
 type MenuItem = {
   label: React.ReactNode;
@@ -16,68 +17,103 @@ type MenuItem = {
   onClick?: () => void;
 };
 
-const menuItems: MenuItem[] = [
-  {
-    label: (
-      <>
-        모임통장에
-        <br />
-        입금
-      </>
-    ),
-    icon: IconTransfer,
-    onClick: () => console.log('메뉴 1 클릭'),
-  },
-  {
-    label: (
-      <>
-        내/모임
-        <br />
-        계좌관리
-      </>
-    ),
-    icon: IconManagement,
-    onClick: () => console.log('메뉴 2 클릭'),
-  },
-  {
-    label: (
-      <>
-        모임 일정
-        <br />
-        둘러보기
-      </>
-    ),
-    icon: IconSchedule,
-    onClick: () => console.log('메뉴 3 클릭'),
-  },
-  {
-    label: (
-      <>
-        지난
-        <br />
-        모임 추억
-      </>
-    ),
-    icon: IconMemory,
-    onClick: () => console.log('메뉴 4 클릭'),
-  },
-  { label: <>모임 결산</>, 
-    icon: IconClosing,
-    onClick: () => console.log('메뉴 5 클릭') },
-  {
-    label: (
-      <>
-        회비
-        <br />
-        입금 현황
-      </>
-    ),
-    icon: IconFeeCheck,
-    onClick: () => console.log('메뉴 6 클릭'),
-  },
-];
-
 const CardMenu: React.FC = () => {
+  const router = useRouter();
+
+  const onClick = (menuKey: string) => {
+    switch (menuKey) {
+      case 'transfer':
+        console.log('모임통장에 입금 클릭');
+        router.push('/transfer');
+        break;
+      case 'management':
+        console.log('내/모임 계좌관리 클릭');
+        router.push('/account-management');
+        break;
+      case 'schedule':
+        console.log('모임 일정 둘러보기 클릭');
+        router.push('/schedule');
+        break;
+      case 'memory':
+        console.log('지난 모임 추억 클릭');
+        router.push('/memory');
+        break;
+      case 'closing':
+        console.log('모임 결산 클릭');
+        router.push('/closing');
+        break;
+      case 'feeCheck':
+        console.log('회비 입금 현황 클릭');
+        router.push('/fee-check');
+        break;
+      default:
+        console.error('잘못된 메뉴 선택');
+    }
+  };
+
+  const menuItems: MenuItem[] = [
+    {
+      label: (
+        <>
+          모임통장에
+          <br />
+          입금
+        </>
+      ),
+      icon: IconTransfer,
+      onClick: () => onClick('transfer'),
+    },
+    {
+      label: (
+        <>
+          내/모임
+          <br />
+          계좌관리
+        </>
+      ),
+      icon: IconManagement,
+      onClick: () => onClick('management'),
+    },
+    {
+      label: (
+        <>
+          모임 일정
+          <br />
+          둘러보기
+        </>
+      ),
+      icon: IconSchedule,
+      onClick: () => onClick('schedule'),
+    },
+    {
+      label: (
+        <>
+          지난
+          <br />
+          모임 추억
+        </>
+      ),
+      icon: IconMemory,
+      onClick: () => onClick('memory')
+    },
+    {
+      label: <>모임 결산</>,
+      icon: IconClosing,
+      onClick: () => onClick('closing'),
+    },
+    {
+      label: (
+        <>
+          회비
+          <br />
+          입금 현황
+        </>
+      ),
+      icon: IconFeeCheck,
+      onClick: () => onClick('fee-check'),
+    },
+  ];
+
   return (
     <div className='grid grid-cols-2 gap-5'>
       {menuItems.map((item, index) => (
@@ -89,12 +125,7 @@ const CardMenu: React.FC = () => {
           <div className='flex flex-col w-full text-[20px]'>
             <div className='h-1/2'>{item.label}</div>
             <div className='h-1/2 flex justify-end items-end'>
-              <Image
-                src={item.icon}
-                alt='입금아이콘'
-                width={70} 
-                height={70}
-              />
+              <Image src={item.icon} alt='입금아이콘' width={70} height={70} />
             </div>
           </div>
         </Card>
