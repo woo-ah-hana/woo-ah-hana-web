d2c8f28ff2809085d114d8c28b1f9a2f1116d00f
yoounyoungheon <iddyoon@gmail.com>
2025-01-13 11:02:21 +0900

    [ui] 로그인 화면 css 수정

--- woo-ah-hana-web/src/app/page.tsx
+++ woo-ah-hana-web/src/app/page.tsx
@@ -5,7 +5,7 @@ import Logo from '@/app/assets/img/logo.png';
 
 export default function Home() {
   return (
-    <div className='h-screen p-10 flex flex-col justify-between'>
+    <div className='px-10 mb-10 flex flex-col'>
       <div className="justify-items-center">
         <Image src={Logo} alt={"우아하나로고"} width={300} height={300} priority/>
       </div>
