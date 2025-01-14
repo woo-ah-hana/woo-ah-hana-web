f54242db096b4f4cfd94548049c61c06e065305c
yoounyoungheon <iddyoon@gmail.com>
2025-01-14 11:31:20 +0900

    [ui] 날짜 상세 모달

---
+++ woo-ah-hana-web/src/app/ui/components/plan/date-detail.modal.tsx
@@ -0,0 +1,35 @@
+import AchromaticButton from "@/app/ui/atom/button/achromatic-button";
+import { Dialog, DialogContent, DialogTrigger } from "@/app/ui/molecule/dialog/dialog";
+import Form from "../../molecule/form/form-index";
+import { updatePlanDate } from "@/app/business/plan/plan-update.service";
+
+interface DateDetailDilogProps{
+  id: string,
+  startDate: string,
+  endDate: string
+}
+
+export async function DateDetailDilog({id, startDate, endDate}: DateDetailDilogProps){
+  // TODO: FormData 활용해서 날짜 업데이트 api 요청
+
+  console.log(id);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <AchromaticButton>DialogTrigger</AchromaticButton>
+      </DialogTrigger>
+      <DialogContent title="모임일정 날짜">
+        <div className="p-5 text-center">
+        <div className="text-lg text-slate-500">
+          {startDate.substring(3,10)}~{endDate.substring(3,10)}
+        </div>
+        <Form id={""} action={updatePlanDate} failMessageControl={"alert"}>
+          <div>
+            <Form.SubmitButton label="수정하기"></Form.SubmitButton>
+          </div>
+        </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
