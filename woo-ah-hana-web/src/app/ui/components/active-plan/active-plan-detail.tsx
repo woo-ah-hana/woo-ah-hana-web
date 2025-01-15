85f41e5c5e759943d3a1e8d2a812fb8945c680fc
yoounyoungheon <iddyoon@gmail.com>
2025-01-15 20:16:50 +0900

    [ui] plan/active page

--- woo-ah-hana-web/src/app/ui/components/active-plan/active-plan-detail.tsx
+++ woo-ah-hana-web/src/app/ui/components/active-plan/active-plan-detail.tsx
@@ -4,29 +4,30 @@ import { IoTimeOutline, IoLocationOutline } from "react-icons/io5";
 import { MdOutlineDescription } from "react-icons/md";
 import { AiOutlineGlobal } from "react-icons/ai";
 
-export function ActivePlanDetail({schedule, time, description, address, link}:ActivePlan){
+export function ActivePlanDetail({date, schedule, time, description, address, link, mapx, mapy}:ActivePlan){
+  console.log(date, mapx, mapy);
   return(
     <main>
       <Card>
-        <CardTitle>
+        <CardTitle className="p-5">
           {schedule}
         </CardTitle>
-        <div className="grid grid-cols-1">
-          <CardContent className="grid grid-cols-[1fr_2fr gap-2]">
-            <div><IoTimeOutline size={50}/></div>
+        <div>
+          <CardContent className="grid grid-cols-[1fr_9fr] gap-2">
+            <div><IoTimeOutline size={20}/></div>
             <div>{time}</div>
           </CardContent>
-          <CardContent className="grid grid-cols-[1fr_2fr gap-2]">
-            <div><MdOutlineDescription size={50}/></div>
+          <CardContent className="grid grid-cols-[1fr_9fr] gap-2">
+            <div><MdOutlineDescription size={20}/></div>
             <div>{description}</div>
           </CardContent>
-          <CardContent className="grid grid-cols-[1fr_2fr gap-2]">
-            <div><IoLocationOutline size={50}/></div>
+          <CardContent className="grid grid-cols-[1fr_9fr] gap-2">
+            <div><IoLocationOutline size={20}/></div>
             <div>{address}</div>
           </CardContent>
-          <CardContent className="grid grid-cols-[1fr_2fr gap-2]">
-            <div><AiOutlineGlobal size={50}/></div>
-            <div>{link}</div>
+          <CardContent className="grid grid-cols-[1fr_9fr] gap-2">
+            <div><AiOutlineGlobal size={20}/></div>
+            <div><a href={`${link}`}>해당 장소 자세히 보기</a></div>
           </CardContent>
         </div>
       </Card>
