f19c44c4a2d9f40cdb23966fa93d876dd833f4bb
yoounyoungheon <iddyoon@gmail.com>
2025-01-15 16:02:49 +0900

    [test] open ai api test

--- woo-ah-hana-web/src/app/page.tsx
+++ woo-ah-hana-web/src/app/page.tsx
@@ -1,14 +1,9 @@
 import LoginForm from "./ui/components/auth/login-form";
 import Image from "next/image";
 import Logo from '@/app/assets/img/logo.png';
-import { CustomAI } from "./business/ai/custom-open-ai";
-
 
 export default async function Home() {
-  const ai = new CustomAI();
-  const result = await ai.turnIntoSearchInput("강남에서 7명이 갈 느낌 좋은 카페 추천해줘..")
-  console.log(result);
-  
+
   return (
     <div className='px-10 mb-10 flex flex-col'>
       <div className="justify-items-center">
