686a466b7e4eb2f208b48d30ef3953e87c9d0438
yoounyoungheon <iddyoon@gmail.com>
2025-01-15 11:45:07 +0900

    [test] ai test

--- woo-ah-hana-web/src/app/business/ai/custom-open-ai.ts
+++ woo-ah-hana-web/src/app/business/ai/custom-open-ai.ts
@@ -1,20 +1,22 @@
 import OpenAI from "openai";
 
 export class CustomAI {
-  private readonly openai = new OpenAI({apiKey: process.env.AI_SECRETE})
+  private readonly openai = new OpenAI({apiKey: `${process.env.AI_SECRETE}`})
   static create(): CustomAI{
     return new CustomAI();
   }
 
-  async extractKeywords(message: string): Promise<string>{
+  async turnIntoSearchInput(message: string): Promise<string>{
     const content: string = `
-    요청 메세지: ${message}
-
-    다음 요청 메세지를 분석해서 검색어로 변형해주세요.
+    다음 요청 메세지를 분석해서 한국어 문자열로 변형해주세요.
     변형된 응답값은 naver 검색엔진의 입력값이 될 것 입니다.
+    응답값 외 다른 매세지는 반환 금지입니다.
 
-    예시: "분위기 좋은 강남 맛집 추천해주세요 ~"
-    응답: "분위기 좋은 강남 맛집"
+    요청 메세지: ${message}
+    응답 형식: string
+
+    요청 메세지 예시: "분위기 좋은 강남 맛집 추천해주세요 ~"
+    응답 예시: "분위기 좋은 강남 맛집"
     `
 
     const response:OpenAI.Chat.Completions.ChatCompletion 
@@ -23,7 +25,7 @@ export class CustomAI {
         messages: [
           {
             role: "system",
-            content: "당신은 검색어 변환기입니다. 자연어를 자연스러운 검색어로 변형합니다."
+            content: "당신은 검색어 변환기입니다. 자연어를 검색엔진에 입력될 검색어로 변형합니다."
           },
           {
             role: 'user', content: content
@@ -32,7 +34,7 @@ export class CustomAI {
       })
 
       const result = response.choices[0].message.content as string;
-      const data = JSON.parse(result);
+      const data = JSON.parse(result) as string;
       return data;
   }
 
