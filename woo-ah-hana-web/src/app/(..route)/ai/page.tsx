cc1c6b38516378c41a06eba9ccef1a63908fdb60
yoounyoungheon <iddyoon@gmail.com>
2025-01-20 13:24:26 +0900

    [feat] 액티브 플랜 만드는 프롬프트 UI 구현

---
+++ woo-ah-hana-web/src/app/(..route)/ai/page.tsx
@@ -0,0 +1,19 @@
+import { RequestActivePlanForm } from "@/app/ui/components/active-plan/requset-active-plan-form";
+import { Card } from "@/app/ui/molecule/card/card";
+
+export default function Home(){
+  
+  return (
+  <main>
+    <div className="flex flex-col gap-3 p-5">
+      
+      <Card className="flex flex-col gap-2 p-5 text-center">
+        <div> 여행 계획 짜주는 AI입니다. </div>
+        <div> 음성 또는 키보드로 요청사항을 입력하세요. </div>
+        <div> AI 요청은 하루에 세번까지만 가능합니다. </div>
+      </Card>
+      <RequestActivePlanForm/>
+    </div>
+  </main>
+  )
+}
