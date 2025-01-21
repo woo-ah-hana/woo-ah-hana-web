faf08a4ee0ca844584fdb193e818afd8a7de3651
yoounyoungheon <iddyoon@gmail.com>
2025-01-21 17:25:59 +0900

    [ui] plan date update dialog ui 구현

--- woo-ah-hana-web/src/app/ui/components/plan/date-detail.modal.tsx
+++ woo-ah-hana-web/src/app/ui/components/plan/date-detail.modal.tsx
@@ -10,14 +10,10 @@ import { updatePlanDate } from "@/app/business/plan/plan-update.service";
 
 interface DateDetailDilogProps {
   id: string;
-  startDate: string;
-  endDate: string;
 }
 
 export function DateDetailDilog({
   id,
-  startDate,
-  endDate,
 }: DateDetailDilogProps) {
   // TODO: FormData 활용해서 날짜 업데이트 api 요청
 
@@ -29,14 +25,20 @@ export function DateDetailDilog({
           수정
         </AchromaticButton>
       </DialogTrigger>
-      <DialogContent title="모임일정 날짜">
+      <DialogContent title="모임일정 변경">
         <div className="p-5 text-center">
-          <div className="text-lg text-slate-500">
-            {startDate.substring(3, 10)}~{endDate.substring(3, 10)}
-          </div>
-          <Form id={""} action={updatePlanDate} failMessageControl={"alert"}>
-            <div className="p-10"></div>
-            <div>
+          <Form id={"update-date"} action={updatePlanDate} failMessageControl={"alert"}>
+            <div className="grid grid-cols-1 text-lg text-slate-500">
+              <div className="grid grid-cols-[2fr_4fr]">
+                <div className="text-left p-3">시작 일:</div>
+                <input type="date" id="start-date" className="border-none"/>
+              </div>
+              <div className="grid grid-cols-[2fr_4fr]">
+                <div className="text-left p-3">종료 일:</div>
+                <input type="date" id="start-date" className="border-none"/>
+              </div>
+            </div>
+            <div className="mt-7">
               <Form.SubmitButton
                 label="수정하기"
                 position="center"
