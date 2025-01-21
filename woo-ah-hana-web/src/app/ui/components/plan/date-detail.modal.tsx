7bd6d2e21d060e73c5abee90772e9dc5a3a86d6c
yoounyoungheon <iddyoon@gmail.com>
2025-01-21 23:44:42 +0900

    [feat] update plan service 구현

--- woo-ah-hana-web/src/app/ui/components/plan/date-detail.modal.tsx
+++ woo-ah-hana-web/src/app/ui/components/plan/date-detail.modal.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/app/ui/molecule/dialog/dialog";
 import Form from "../../molecule/form/form-index";
 import { updatePlanDate } from "@/app/business/plan/plan-update.service";
+import { FormSubmitButton } from "../../molecule/form/form-submit-button";
 
 interface DateDetailDilogProps {
   id: string;
@@ -27,22 +28,20 @@ export function DateDetailDilog({
       </DialogTrigger>
       <DialogContent title="모임일정 변경">
         <div className="p-5 text-center">
-          <Form id={"update-date"} action={updatePlanDate} failMessageControl={"alert"}>
-            <div className="grid grid-cols-1 text-lg text-slate-500">
+          <Form id="updatedate" action={updatePlanDate} failMessageControl={"alert"}>
+            <Form.TextInput value={id} id='id' className="hidden" placeholder=""/>
+            <div className="grid grid-cols-1 gap-2 text-lg text-slate-500">
               <div className="grid grid-cols-[2fr_4fr]">
                 <div className="text-left p-3">시작 일:</div>
-                <input type="date" id="start-date" className="border-none"/>
+                <input type="date" name="start-date" id="start-date" className="border-none"/>
               </div>
               <div className="grid grid-cols-[2fr_4fr]">
                 <div className="text-left p-3">종료 일:</div>
-                <input type="date" id="start-date" className="border-none"/>
+                <input type="date" name="end-date" id="end-date" className="border-none"/>
               </div>
             </div>
             <div className="mt-7">
-              <Form.SubmitButton
-                label="수정하기"
-                position="center"
-              ></Form.SubmitButton>
+              <FormSubmitButton label="수정하기" position="center"/>
             </div>
           </Form>
         </div>
