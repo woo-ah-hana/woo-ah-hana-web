ac4c1807f742b4139a4fbbd691160c5255bbdae2
yoounyoungheon <iddyoon@gmail.com>
2025-01-23 13:22:09 +0900

    [feat] 회비 안낸 사람들 조회하는 api 연동

--- woo-ah-hana-web/src/app/(..route)/fee-status/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/fee-status/page.tsx
@@ -1,25 +1,19 @@
-'use client';
-
 import Image from 'next/image';
 import ProfileImage from '../../assets/img/profile.jpg';
 import AchromaticButton from '@/app/ui/atom/button/achromatic-button';
+import { getCommunityFeeStatus, MemberFeeStatus } from '@/app/business/community/community.service';
 
-//임시 멤버 데이터
-const members = [
-  { name: '김하나', fee: '0' },
-  { name: '홍창기', fee: '50000' },
-  { name: '문보경', fee: '100000' },
-  { name: '박해민', fee: '100000' },
-  { name: '김현수', fee: '100000' },
-];
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const response = await getCommunityFeeStatus(searchParams.community as string)
+  const communityFeeStatus = response.data;
+  const paidMembers = communityFeeStatus?.paidMembers as MemberFeeStatus[];
+  const unpaidMembers = communityFeeStatus?.unpaidMembers as MemberFeeStatus[];
+  const totalMembers = paidMembers.concat(unpaidMembers);
 
-const unpaidMembers = members.filter(member => parseInt(member.fee) < 100000);
-
-export default function FeeStatus() {
-    const handleRequestButton = () => {
-        console.log('회비 요청하기');
-    }
-    
   return (
     <div className='p-5 flex flex-col gap-8'>
       <div className='flex flex-col justify-center items-start'>
@@ -32,19 +26,19 @@ export default function FeeStatus() {
             <div className='flex justify-center items-center gap-8'>
               <Image
                 src={ProfileImage}
-                alt={`${member.name} 프로필`}
+                alt={`${member.memberName} 프로필`}
                 className='w-12 h-12 rounded-full object-cover'
               />
-              <div className='text-gray-800'>{member.name}</div>
+              <div className='text-gray-800'>{member.memberName}</div>
             </div>
-            <AchromaticButton onClick={handleRequestButton}>회비 요청</AchromaticButton>
+            <AchromaticButton>회비 요청</AchromaticButton>
           </div>
         ))}
       </div>
       <hr></hr>
       <div className='flex flex-col justify-center items-start'>
         <div className='text-[18px] mb-4'>모임 멤버</div>
-        {members.map((member, index) => (
+        {totalMembers.map((member, index) => (
           <div
             key={index}
             className='flex items-center justify-between gap-6 p-2 w-full'
@@ -52,12 +46,12 @@ export default function FeeStatus() {
             <div className='flex justify-center items-center gap-8'>
               <Image
                 src={ProfileImage}
-                alt={`${member.name} 프로필`}
+                alt={`${member.memberName} 프로필`}
                 className='w-12 h-12 rounded-full object-cover'
               />
-              <div>{member.name}</div>
+              <div>{member.memberName}</div>
             </div>
-            <div><span className='text-[19px]'>{member.fee}</span> 원</div>
+            <div><span className='text-[19px]'>{member.amount}</span> 원</div>
           </div>
         ))}
       </div>
