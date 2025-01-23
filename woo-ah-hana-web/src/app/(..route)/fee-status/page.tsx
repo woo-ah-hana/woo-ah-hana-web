5f69f5e6e550eafe43f1e14e8927e1c737f46e32
yoounyoungheon <iddyoon@gmail.com>
2025-01-23 13:39:59 +0900

    [feat] 페이지 내 cqrs에 따라 서버 컴포넌트와 클라이언트 컴포넌트를 나눔

--- woo-ah-hana-web/src/app/(..route)/fee-status/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/fee-status/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image';
-import ProfileImage from '../../assets/img/profile.jpg';
-import AchromaticButton from '@/app/ui/atom/button/achromatic-button';
 import { getCommunityFeeStatus, MemberFeeStatus } from '@/app/business/community/community.service';
+import { UnpaidMemberList } from '@/app/ui/components/fee-status/unpaid-members';
+import { PaidMemberList } from '@/app/ui/components/fee-status/paid-members';
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  // TODO: API 응답값이 수정되면, 리팩터링 필요
   const response = await getCommunityFeeStatus(searchParams.community as string)
   const communityFeeStatus = response.data;
   const paidMembers = communityFeeStatus?.paidMembers as MemberFeeStatus[];
@@ -16,45 +16,9 @@ export default async function Home({
 
   return (
     <div className='p-5 flex flex-col gap-8'>
-      <div className='flex flex-col justify-center items-start'>
-        <div className='text-[18px] mb-4'>안 낸 멤버</div>
-        {unpaidMembers.map((member, index) => (
-          <div
-            key={index}
-            className='flex items-center justify-between gap-6 p-2 w-full'
-          >
-            <div className='flex justify-center items-center gap-8'>
-              <Image
-                src={ProfileImage}
-                alt={`${member.memberName} 프로필`}
-                className='w-12 h-12 rounded-full object-cover'
-              />
-              <div className='text-gray-800'>{member.memberName}</div>
-            </div>
-            <AchromaticButton>회비 요청</AchromaticButton>
-          </div>
-        ))}
-      </div>
+      <UnpaidMemberList unpaidMembers={unpaidMembers}/>
       <hr></hr>
-      <div className='flex flex-col justify-center items-start'>
-        <div className='text-[18px] mb-4'>모임 멤버</div>
-        {totalMembers.map((member, index) => (
-          <div
-            key={index}
-            className='flex items-center justify-between gap-6 p-2 w-full'
-          >
-            <div className='flex justify-center items-center gap-8'>
-              <Image
-                src={ProfileImage}
-                alt={`${member.memberName} 프로필`}
-                className='w-12 h-12 rounded-full object-cover'
-              />
-              <div>{member.memberName}</div>
-            </div>
-            <div><span className='text-[19px]'>{member.amount}</span> 원</div>
-          </div>
-        ))}
-      </div>
+      <PaidMemberList members={totalMembers}/>
     </div>
   );
 }
