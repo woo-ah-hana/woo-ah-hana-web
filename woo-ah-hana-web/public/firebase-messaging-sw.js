dab2d0ae2b7d3b4ee5b5094b631ca5cb0f5aca3b
yoounyoungheon <iddyoon@gmail.com>
2025-01-27 19:29:16 +0900

    [config] 환경 변수 파일 수정

--- woo-ah-hana-web/public/firebase-messaging-sw.js
+++ woo-ah-hana-web/public/firebase-messaging-sw.js
@@ -4,14 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-comp
 
 // Your web app's Firebase configuration
 const config = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_MESSAGE_APP_ID}`,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_APP_ID,
 };
-
 // Initialize Firebase
 firebase.initializeApp(config);
 
--- woo-ah-hana-web/src/app/ui/components/notification/index.tsx
+++ woo-ah-hana-web/src/app/ui/components/notification/index.tsx
@@ -11,19 +11,19 @@ const NotificationIndex = ():React.JSX.Element => {
       if (permission !== 'granted') return
 
       const config = {
-        apiKey: `${process.env.FIREBASE_API_KEY}`,
-        authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-        projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-        storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-        messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
-        appId: `${process.env.FIREBASE_MESSAGE_APP_ID}`,
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_APP_ID,
       };
 
       const firebaseApp = initializeApp(config)
 
       const messaging: Messaging = getMessaging(firebaseApp)
 
-      const vapidKey = `${process.env.FIREBASE_VAPID_KEY}`
+      const vapidKey = process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY
       const currentToken = await getToken(messaging, { vapidKey })
 
       if (currentToken) {
