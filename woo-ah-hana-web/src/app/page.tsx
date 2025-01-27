2bf2b14507a3b569dcca57e48ac4cc46dc738c20
yoounyoungheon <iddyoon@gmail.com>
2025-01-27 18:43:08 +0900

    [config] firebase 연동

--- woo-ah-hana-web/src/app/page.tsx
+++ woo-ah-hana-web/src/app/page.tsx
@@ -1,11 +1,13 @@
 import LoginForm from "./ui/components/auth/login-form";
 import Image from "next/image";
 import Logo from '@/app/assets/img/logo.png';
+import NotificationIndex from "./ui/components/notification";
 
 export default async function Home() {
 
   return (
     <div className='px-10 mb-10 flex flex-col'>
+      <NotificationIndex/>
       <div className="justify-items-center">
         <Image src={Logo} alt={"우아하나로고"} width={300} height={300} priority/>
       </div>
