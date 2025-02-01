5af58d7c945cc8d0a3a405ef373e72872df16405
yoounyoungheon <iddyoon@gmail.com>
2025-02-01 17:18:10 +0900

    [refactor] signup login service 분리

---
+++ woo-ah-hana-web/src/app/business/auth/signup.service.ts
@@ -0,0 +1,89 @@
+import { z } from 'zod';
+import { generateFCMToken } from '../notification/fcm.service';
+import { FormState } from '@/app/ui/molecule/form/form-root';
+import { BankName, convertBankNameToCode } from '@/app/utils/convert';
+import { API_PATH } from '@/app/utils/http/api-query';
+import { HttpError } from '@/app/utils/http/http-error';
+import { httpErrorHandler } from '@/app/utils/http/http-error-handler';
+import { AxiosError } from 'axios';
+
+const SignUpFormSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  name: z.string(),
+  phoneNumber: z.string(),
+  accountNumber: z.string(),
+  bankTranId: z.string(),
+  fcmToken: z.string()
+})
+
+type SignUpRequestBody = z.infer<typeof SignUpFormSchema>
+
+export async function signUp(prevState: FormState, formData: FormData):Promise<FormState> {
+  const fcmToken = await generateFCMToken();
+  console.log(fcmToken);
+
+  const validatedFields = SignUpFormSchema.safeParse({
+    username: formData.get("username") as string,
+    password: formData.get("password") as string,
+    name: formData.get("name") as string,
+    phoneNumber: formData.get("username") as string,
+    accountNumber: formData.get("accountNumber") as string,
+    bankTranId: convertBankNameToCode(formData.get("bankTranId") as BankName),
+    fcmToken: fcmToken
+  })
+
+  if (!validatedFields.success) {
+    return {
+      isSuccess: false,
+      isFailure: true,
+      validationError: validatedFields.error.flatten().fieldErrors,
+      message: "양식에 맞춰 다시 입력해주세요.",
+    };
+  }
+
+  const body: SignUpRequestBody = { ...validatedFields.data };
+
+  try{
+    const response = await fetch(`${API_PATH}/member/login`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+      throw new HttpError(response.status, "서버에러");
+    }
+
+    const result = await response.json();
+    console.log(result);
+
+    return {
+      isSuccess: true,
+      isFailure: false,
+      validationError: {},
+      message: "회원가입에 성공했습니다.",
+    };
+
+  }catch(error){
+    console.log(error)
+    if(error instanceof Error && error instanceof AxiosError){
+      const exception = await httpErrorHandler(error);
+      return {
+        isSuccess: false,
+        isFailure: true,
+        validationError: {},
+        message: exception.message,
+      };
+    }else{
+      return {
+        isSuccess: false,
+        isFailure: true,
+        validationError: {},
+        message: '회원가입에 실패했습니다. 다시 시도해주세요.'
+      }
+    }
+  }
+}
---
+++ woo-ah-hana-web/src/app/ui/components/auth/signup-form.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { authenticate } from "@/app/business/auth/login.service";
+import Form from "../../molecule/form/form-index";
+import { useRouter } from "next/navigation";
+
+
+export default function SignupForm(){
+  const router = useRouter();
+
+  return(
+    <main>
+      <Form id='login' action={authenticate} onSuccess={()=>{router.push('/home')}} failMessageControl={"alert"}>
+        <div className="grid grid-cols-1 gap-5">
+          <Form.TextInput id="username" label="전화번호" placeholder=""/>
+          <Form.TextInput id="name" label="이름" placeholder=""/>
+          <Form.PasswordInput id="password" label="비밀번호" placeholder=""/>
+          <div>
+            <Form.SubmitButton label="로그인" position="center" className="w-full h-12"/>
+          </div>
+        </div>
+      </Form>
+    </main>
+  )
+}
