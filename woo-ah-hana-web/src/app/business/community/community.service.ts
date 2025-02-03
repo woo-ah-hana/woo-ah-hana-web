f8569668c17d7a203b05329b4dc22f3939c17414
yoounyoungheon <iddyoon@gmail.com>
2025-02-03 13:21:58 +0900

    .

--- woo-ah-hana-web/src/app/business/community/community.service.ts
+++ woo-ah-hana-web/src/app/business/community/community.service.ts
@@ -3,18 +3,15 @@ import { APIResponseType, instance } from "@/app/utils/http";
 import { API_PATH } from "@/app/utils/http/api-query";
 import { InternetServerError } from "@/app/utils/http/http-error";
 import { Community } from "./community";
-
 export interface CommunityResponseDTO {
   communityId: string;
   name: string;
 }
-
 export interface CommunityInfoResponseDTO {
   name: string;
   accountNumber: string;
   balance: number;
 }
-
 export interface GetMembersDto {
   id: string;
   name: string;
@@ -23,22 +20,19 @@ export interface Member{
   id: string,
   name: string
 }
-
 export interface ValidationCodeRequestDTO{
   bankTranId: string;
   accountNumber: string;
 }
-
 export interface MemberFeeStatus{
   memberName: string;
+  memberId: string;
   amount: number
 }
-
 export interface CommunityFeeStatus{
   paidMembers: MemberFeeStatus[];
   unpaidMembers: MemberFeeStatus[];
 }
-
 export interface CreateCommunityRequestDTO{
   name: string;
   accountNumber: string;
@@ -47,7 +41,6 @@ export interface CreateCommunityRequestDTO{
   fee: number;
   feePeriod: number;
 }
-
 export interface Transfer{
   tranDate: string;
   tranTime: string;
@@ -58,13 +51,11 @@ export interface Transfer{
   afterBalanceAmt: string;
   branchName: string;
 }
-
 export interface getRecapRequestDTO{
   communityId: string;
   fromDate: string;
   toDate: string;
 }
-
 export interface getRecapResponseDTO{
   planTitleList: string[];
   numberOfPlans: number;
@@ -76,11 +67,14 @@ export interface getRecapResponseDTO{
   highestPlanName: string;
   highestPlanExpense: number;
 }
-
+export interface RegisterCommunityResponseDTO{
+  memberName: string;
+  communityName: string;
+  success: boolean
+}
 export async function getCommunityList(): Promise<APIResponseType<CommunityResponseDTO[]>> {
   try {
     const response = await instance.get(`${API_PATH}/community/list`);
-
     if (response.status === 500) {
       throw new InternetServerError({
         message: "서버가 불안정합니다. 잠시후 시도해주세요.",
@@ -88,9 +82,7 @@ export async function getCommunityList(): Promise<APIResponseType<CommunityRespo
         response: response.data,
       });
     }
-
     const data: CommunityResponseDTO[] = response.data;
-
     return {
       isSuccess: true,
       isFailure: false,
@@ -105,7 +97,6 @@ export async function getCommunityList(): Promise<APIResponseType<CommunityRespo
     };
   }
 }
-
 export async function getCommunity(communityId: string):Promise<APIResponseType<Community>>{
   const response = await instance.get(`${API_PATH}/community/${communityId}`);
   if (response.status === 500) {
@@ -130,7 +121,6 @@ export async function getCommunity(communityId: string):Promise<APIResponseType<
     };
   }
 }
-
 export async function getCommunityInfo(
   communityId: string
 ): Promise<APIResponseType<CommunityInfoResponseDTO>> {
@@ -138,7 +128,6 @@ export async function getCommunityInfo(
     const response = await instance.get(
       `${API_PATH}/community/info?communityId=${communityId}`
     );
-
     if (response.status === 500) {
       throw new InternetServerError({
         message: "서버가 불안정합니다. 잠시후 시도해주세요.",
@@ -146,9 +135,7 @@ export async function getCommunityInfo(
         response: response.data,
       });
     }
-
     const data: CommunityInfoResponseDTO = response.data;
-
     return {
       isSuccess: true,
       isFailure: false,
@@ -163,14 +150,12 @@ export async function getCommunityInfo(
     };
   }
 }
-
 export async function getMembers(
   communityId: string
 ): Promise<APIResponseType<GetMembersDto[]>> {
   const response = await instance.get(
     `${API_PATH}/community/member-list/${communityId}`
   );
-
   if (response.status == 500) {
     throw new InternetServerError({
       message: "서버가 불안정합니다. 잠시후 시도해주세요.",
@@ -178,10 +163,8 @@ export async function getMembers(
       response: response.data,
     });
   }
-
   try {
     const data: GetMembersDto[] = response.data;
-
     return {
       isSuccess: true,
       isFailure: false,
@@ -195,7 +178,6 @@ export async function getMembers(
     });
   }
 }
-
 export async function getCommunityMembers(communityId: string): Promise<APIResponseType<Member[]>> {
   console.log(`${API_PATH}/community/member-list/${communityId}`)
   const response = await instance.get(`${API_PATH}/community/member-list/${communityId}`);
@@ -206,7 +188,6 @@ export async function getCommunityMembers(communityId: string): Promise<APIRespo
       response: response.data
     });
   }
-
   try{
     return {
       isSuccess: true,
@@ -222,11 +203,9 @@ export async function getCommunityMembers(communityId: string): Promise<APIRespo
     }
   }
 }
-
 export async function validateAccount(requestBody: ValidationCodeRequestDTO): Promise<APIResponseType<string>> {
   try {
     const response = await instance.post(`${API_PATH}/community/send-code`, requestBody);
-
     if (response.status === 500) {
       throw new InternetServerError({
         message: "서버가 불안정합니다. 잠시 후 다시 시도해주세요.",
@@ -234,7 +213,6 @@ export async function validateAccount(requestBody: ValidationCodeRequestDTO): Pr
         response: response.data,
       });
     }
-
     return {
       isSuccess: true,
       isFailure: false,
@@ -247,11 +225,9 @@ export async function validateAccount(requestBody: ValidationCodeRequestDTO): Pr
     };
   }
 }
-
 export async function createCommunity(requestBody: CreateCommunityRequestDTO): Promise<APIResponseType<string>> {
   try {
     const response = await instance.post(`${API_PATH}/community/new`, requestBody);
-
     if (response.status === 500) {
       throw new InternetServerError({
         message: "서버가 불안정합니다. 잠시 후 다시 시도해주세요.",
@@ -265,16 +241,15 @@ export async function createCommunity(requestBody: CreateCommunityRequestDTO): P
       data: response.data as string
     }
     }
-catch (error) {
-    console.error(error);
-    return {
-      isSuccess: false,
-      isFailure: true,
-      data: undefined
-    };
+  catch (error) {
+      console.error(error);
+      return {
+        isSuccess: false,
+        isFailure: true,
+        data: undefined
+      };
   }
 }
-
 export async function getCommunityFeeStatus(communityId: string):Promise<APIResponseType<CommunityFeeStatus>> {
   const response = await instance.post(`${API_PATH}/community/feeStatus`, {communityId});
   try{
@@ -292,12 +267,9 @@ export async function getCommunityFeeStatus(communityId: string):Promise<APIResp
     }
   }
 }
-
-
 export async function getCommunityTransferRecords(communityId: string, recentMonth: number):Promise<APIResponseType<Transfer[]>> {
   const requestBody = {communityId, recentMonth};
   const response = await instance.post(`${API_PATH}/community/trsfRecords`, requestBody);
-
   if (response.status === 500) {
     throw new InternetServerError({
       message: "서버가 불안정합니다. 잠시 후 다시 시도해주세요.",
@@ -305,7 +277,6 @@ export async function getCommunityTransferRecords(communityId: string, recentMon
       response: response.data,
     });
   }
-
   try{
     return {
       isSuccess: true,
@@ -321,13 +292,11 @@ export async function getCommunityTransferRecords(communityId: string, recentMon
     }
   }
 }
-
 export async function getRecap(requestBody: getRecapRequestDTO): Promise<APIResponseType<getRecapResponseDTO>> {
   try {
     const response = await instance.post(
       `${API_PATH}/community/expense-info`, requestBody
     );
-
     if (response.status === 500) {
       throw new InternetServerError({
         message: "서버가 불안정합니다. 잠시후 시도해주세요.",
@@ -335,9 +304,7 @@ export async function getRecap(requestBody: getRecapRequestDTO): Promise<APIResp
         response: response.data,
       });
     }
-
     const data: getRecapResponseDTO = response.data;
-
     return {
       isSuccess: true,
       isFailure: false,
@@ -350,5 +317,41 @@ export async function getRecap(requestBody: getRecapRequestDTO): Promise<APIResp
       isFailure: true,
       data: undefined,
     };
+  }
+}
+export async function changeFeeInfo(communityId: string, fee: number, feePeriod: number) :Promise<APIResponseType<string>>{
+  try{
+    const response = await instance.post(`${API_PATH}/community/account/changeFeeInfo`, {communityId, fee, feePeriod});
+    const data:string = response.data
+    return {
+      isSuccess: false,
+      isFailure: true,
+      data: data
+    }
+  }catch(error){
+    console.log(error);
+    return{
+      isSuccess: false,
+      isFailure: true,
+      data: undefined
+    }
+  }
+}
+export async function registerCommunity(memberId: string, communityId: string) :Promise<APIResponseType<RegisterCommunityResponseDTO>> {
+  try{
+    const response = await instance.post(`${API_PATH}/community/register`, {memberId, communityId});
+    const data: RegisterCommunityResponseDTO = response.data
+    return {
+      isSuccess: false,
+      isFailure: true,
+      data: data
+    }
+  }catch(error){
+    console.log(error);
+    return{
+      isSuccess: false,
+      isFailure: true,
+      data: undefined
+    }
   }
 }
