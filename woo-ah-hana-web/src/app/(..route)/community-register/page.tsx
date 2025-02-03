a4a232cff7536d410823053a561fd19d7e078586
codnscodns <dnstm1119@naver.com>
2025-02-03 15:12:56 +0900

    [fix] 커뮤니티 추가 페이지 뒤로가기 경로 수정

--- woo-ah-hana-web/src/app/(..route)/community-register/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/community-register/page.tsx
@@ -3,10 +3,14 @@ import Image from 'next/image';
 import IconPlus from '../../assets/img/icon-plus-circle.svg';
 import AchromaticButton from '@/app/ui/atom/button/achromatic-button';
 import Link from 'next/link';
+import Header from '@/app/ui/components/header';
 
 export default function CommunityRegister() {
   return (
-    <div className='p-5 flex flex-col gap-8'>
+    <div className='flex flex-col'>
+      <Header title='뒤로가기' link='/'/>
+      <div className='p-5 flex flex-col gap-8'>
+      
       <Link href={'/community-register/form'}>
         <Card className='h-40 flex justify-center items-center'>
           <Image src={IconPlus} alt='추가버튼' className='w-10' />
@@ -18,5 +22,7 @@ export default function CommunityRegister() {
         </AchromaticButton>
       </Link>
     </div>
+    </div>
+    
   );
 }
