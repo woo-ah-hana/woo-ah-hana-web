f8d646c14a7734974635acf8ce6f8d094219921f
codnscodns <dnstm1119@naver.com>
2025-02-04 14:50:50 +0900

    [ui] 모임결산 이미지url 추가

--- woo-ah-hana-web/src/app/(..route)/memory/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/memory/page.tsx
@@ -1,8 +1,5 @@
 import { MemoryListItem } from "@/app/ui/components/memory/memory-list-item";
 import { getCompletedPlans } from "@/app/business/memory/memory.service";
-import AchromaticButton from "@/app/ui/atom/button/achromatic-button";
-import { IoAdd } from "react-icons/io5";
-import Link from "next/link";
 import Header from "@/app/ui/components/header";
 
 export default async function Home({
@@ -42,13 +39,6 @@ export default async function Home({
           <div className="grid grid-rows-1 gap-3">
             {plans ? PlansView : <div>데이터가 존재하지 않습니다.</div>}
           </div>
-        </div>
-        <div className="fixed bottom-5 right-5 mb-5 flex justify-end items-end">
-          <Link href={"plan/set"}>
-            <AchromaticButton className="h-14 w-14 rounded-full">
-              <IoAdd color="white" size={40} />
-            </AchromaticButton>
-          </Link>
         </div>
       </div>
     </main>
---
+++ woo-ah-hana-web/src/app/ui/components/memory/memory-none.tsx
@@ -0,0 +1,8 @@
+export default function MemoryNone(){
+    return(
+        <div className="flex flex-col justify-center items-center">
+            <div>일정을 생성하고 추억을 만들어보세요!</div>
+
+        </div>
+    )
+}
