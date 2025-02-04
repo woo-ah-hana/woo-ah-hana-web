10e23a2ffe70a191669ef9c4e6395ecfeba26656
codnscodns <dnstm1119@naver.com>
2025-02-04 15:56:49 +0900

    [ui] 모임일정, 추억 없는 경우 ui 추가

--- woo-ah-hana-web/src/app/(..route)/memory/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/memory/page.tsx
@@ -1,6 +1,7 @@
 import { MemoryListItem } from "@/app/ui/components/memory/memory-list-item";
 import { getCompletedPlans } from "@/app/business/memory/memory.service";
 import Header from "@/app/ui/components/header";
+import MemoryNone from "@/app/ui/components/memory/memory-none";
 
 export default async function Home({
   searchParams,
@@ -12,7 +13,6 @@ export default async function Home({
 
   const getPlansResponse = await getCompletedPlans(communityId);
   const plans = getPlansResponse.data;
-  console.log(plans);
 
   const PlansView: React.ReactNode[] | undefined = plans?.map((item, index) => {
     return (
@@ -37,7 +37,8 @@ export default async function Home({
         <Header title="지난 모임 추억" link="/home" />
         <div className="flex-1 overflow-y-auto p-5">
           <div className="grid grid-rows-1 gap-3">
-            {plans ? PlansView : <div>데이터가 존재하지 않습니다.</div>}
+            {/* {plans && plans === undefined ? PlansView : <div>데이터가 존재하지 않습니다.</div>} */}
+            {plans && plans === undefined ? PlansView : <MemoryNone />}
           </div>
         </div>
       </div>
--- woo-ah-hana-web/src/app/ui/components/memory/memory-none.tsx
+++ woo-ah-hana-web/src/app/ui/components/memory/memory-none.tsx
@@ -1,8 +1,13 @@
-export default function MemoryNone(){
-    return(
-        <div className="flex flex-col justify-center items-center">
-            <div>일정을 생성하고 추억을 만들어보세요!</div>
+import Image from 'next/image';
+import IconNone from '../../../assets/img/icon-none.png';
 
-        </div>
-    )
+export default function MemoryNone() {
+  return (
+    <div className='h-[600px] flex flex-col justify-center items-center gap-3 text-xl font-semibold'>
+      <Image src={IconNone} alt='noneIcon' style={{width:'120px', height:'120px'}}/>
+      <div>추억할 모임 일정이 없습니다.</div>
+      <div>일정을 생성하고, </div>
+      <div>지난 일정을 추억해보세요!</div>
+    </div>
+  );
 }
