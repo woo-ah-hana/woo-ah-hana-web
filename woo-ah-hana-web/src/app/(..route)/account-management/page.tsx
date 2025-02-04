3247c4713fe86cc1d09a04a50fb151b523fbe0a2
codnscodns <dnstm1119@naver.com>
2025-02-04 09:46:46 +0900

    [feat] memberInfo api 연동 및 계주일때만 회비 설정 화면 조회

--- woo-ah-hana-web/src/app/(..route)/account-management/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/account-management/page.tsx
@@ -1,10 +1,15 @@
-import ToggleButton from "@/app/ui/atom/toggle/toggle-button";
-import Header from "@/app/ui/components/header";
-import { getCommunity, getCommunityMembers, Member} from "@/app/business/community/community.service";
-import { AccountManagementMain } from "@/app/ui/components/account-management/account-management-main";
-import { getMyAccount } from "@/app/business/account/account.service";
-import { AccountManagementManager } from "@/app/ui/components/account-management/account-management-manager";
-import { AccountManagementMembers } from "@/app/ui/components/account-management/account-management-members";
+import ToggleButton from '@/app/ui/atom/toggle/toggle-button';
+import Header from '@/app/ui/components/header';
+import {
+  getCommunity,
+  getCommunityMembers,
+  Member,
+} from '@/app/business/community/community.service';
+import { AccountManagementMain } from '@/app/ui/components/account-management/account-management-main';
+import { getMyAccount } from '@/app/business/account/account.service';
+import { AccountManagementManager } from '@/app/ui/components/account-management/account-management-manager';
+import { AccountManagementMembers } from '@/app/ui/components/account-management/account-management-members';
+import { getMemberInfo } from '@/app/business/member/member.service';
 
 export default async function AccountManagement({
   searchParams,
@@ -12,39 +17,48 @@ export default async function AccountManagement({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const community = (await getCommunity(searchParams.id as string)).data;
-  const myAccount   = (await getMyAccount()).data;
-  const communityMembers = (await getCommunityMembers(searchParams.id as string)).data;
-  
+  const myAccount = (await getMyAccount()).data;
+  const communityMembers = (
+    await getCommunityMembers(searchParams.id as string)
+  ).data;
+  const myInfo = (await getMemberInfo()).data;
+
   return (
-    <div className="h-full flex flex-col">
-      <Header title="계좌 관리하기" link={`/home?id=${searchParams.id}`} />
-      <div className="p-5 flex flex-col gap-7">
-        <div className="flex flex-col gap-5">
-          <div className="text-[20px]">내 출금 계좌</div>
-          <AccountManagementMain 
+    <div className='h-full flex flex-col'>
+      <Header title='계좌 관리하기' link={`/home?id=${searchParams.id}`} />
+      <div className='p-5 flex flex-col gap-7'>
+        <div className='flex flex-col gap-5'>
+          <div className='text-[20px]'>내 출금 계좌</div>
+          <AccountManagementMain
             bankName={myAccount?.name as string}
             accountNumber={myAccount?.accountNumber as string}
-            accountBalance={myAccount?.amount as number} 
-            fee={community?.fee as number} 
+            accountBalance={myAccount?.amount as number}
+            fee={community?.fee as number}
             feePeriod={community?.feePeriod as number}
           />
         </div>
 
-        <div className="flex flex-col gap-5">
-          <div className="flex items-center justify-between">
+        <div className='flex flex-col gap-5'>
+          <div className='flex items-center justify-between'>
             <div>입출금 알림</div>
-            <ToggleButton text={""} />
+            <ToggleButton text={''} />
           </div>
         </div>
-        <hr className="bg-gray-800 my-3" />
+        <hr className='bg-gray-800 my-3' />
 
-        <AccountManagementManager 
-          fee={community?.fee as number} 
-          feePeriod={community?.feePeriod as number}
+        {myInfo?.id === community?.managerId && (
+          <>
+            <AccountManagementManager
+              fee={community?.fee as number}
+              feePeriod={community?.feePeriod as number}
+            />
+            <hr className='bg-gray-800 my-3' />
+          </>
+        )}
+
+        <AccountManagementMembers
+          communityMembers={communityMembers as Member[]}
         />
-        <hr className="bg-gray-800 my-3" />
-
-        <AccountManagementMembers communityMembers={communityMembers as Member[]}/>
       </div>
     </div>
   );
--- woo-ah-hana-web/src/app/business/member/member.service.ts
+++ woo-ah-hana-web/src/app/business/member/member.service.ts
@@ -4,10 +4,36 @@ import { APIResponseType, instance } from "@/app/utils/http";
 import { API_PATH } from "@/app/utils/http/api-query";
 import { Member } from "../community/community.service";
 
+export interface MemberInfoResponseDTO{
+  id: string;
+  username: string;
+  name: string;
+  phoneNumber: string;
+}
+
 export async function inquiryMember(phoneNumber: string) :Promise<APIResponseType<Member>>{
   try{
     const response = await instance.get(`${API_PATH}/member/inquiry?id=${phoneNumber}`);
     const data:Member = response.data
+    return {
+      isSuccess: false,
+      isFailure: true,
+      data: data
+    }
+  }catch(error){
+    console.log(error);
+    return{
+      isSuccess: false,
+      isFailure: true,
+      data: undefined
+    }
+  }
+}
+
+export async function getMemberInfo() :Promise<APIResponseType<MemberInfoResponseDTO>>{
+  try{
+    const response = await instance.get(`${API_PATH}/member/info`);
+    const data:MemberInfoResponseDTO = response.data
     return {
       isSuccess: false,
       isFailure: true,
