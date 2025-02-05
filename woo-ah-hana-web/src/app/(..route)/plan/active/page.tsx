394fe55d15772413437ec1482e1c39dc4884a861
codnscodns <dnstm1119@naver.com>
2025-02-05 15:35:13 +0900

    [ui] ai 일정 상세보기 페이지 헤더 추가

--- woo-ah-hana-web/src/app/(..route)/plan/active/page.tsx
+++ woo-ah-hana-web/src/app/(..route)/plan/active/page.tsx
@@ -1,11 +1,13 @@
 import { ActivePlan } from "@/app/business/plan/active-plan"
 import { getActivePlans } from "@/app/business/plan/active-plan.service";
 import { ActivePlanDetail } from "@/app/ui/components/active-plan/active-plan-detail"
+import Header from "@/app/ui/components/header";
 import Map from "@/app/ui/components/map/map"
 
 export default async function Home({searchParams}:{searchParams: { [key: string]: string | string[] | undefined }}){
   const planId = searchParams.id;
   const date = searchParams.date;
+  const communityId = searchParams.communityId;
   const getActivePlansResponse = await getActivePlans(planId as string);
   const activePlans = getActivePlansResponse.data?.filter((item)=>{return item.date===date}) as ActivePlan[];
   
@@ -27,6 +29,7 @@ export default async function Home({searchParams}:{searchParams: { [key: string]
 
   return(
     <main>
+      <Header title="AI 일정 상세보기" link={`/plan/detail?community=${communityId}&id=${planId}`}/>
       <div className="p-5 grid grid-cols-1 gap-3">
         {/* TODO: 지도 로딩 중일 경우, 렌딩 페이지 작업 */}
         <Map loc={activePlans.map((item)=>{return item.address})}/>
